ea8a78ab696d0efa1e1f3aa82e10fddee68947fa
BR-BelalRezk <belalrezk.developer@gmail.com>
2024-08-15 11:53:34 +0300

    publish

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AI startup",
+  description: "AI startup landing page",
+  icons: { icon: "/logo.svg" },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(inter.className, "antialiased bg-black text-white")}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/components/home/CTA.tsx
@@ -0,0 +1,6 @@
+
+export default function CTA() {
+  return (
+    <section>CTA</section>
+  )
+}
